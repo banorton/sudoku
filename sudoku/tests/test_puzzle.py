e12acd82c55ab9af7c6f967cbae763ad6e4ed72a
Brandon <banorton1@gmail.com>
2023-11-13 19:24:35 -0500

    Added test for __setitem__ dunder in puzzle backend.

--- sudoku/tests/test_puzzle.py
+++ sudoku/tests/test_puzzle.py
@@ -1,19 +1,36 @@
-from sudoku.puzzle import Cell
+from sudoku.puzzle import Cell, Puzzle_Backend
+from pytest import fixture
 
 
-def testCellNotesChangeAfterSettingValue():
-    c = Cell(pos=(0, 0), box=0)
-    assert c.notes == {1, 2, 3, 4, 5, 6, 7, 8, 9}
-    c.val = 1
-    assert c.notes == {1}
+# Cell
+@fixture
+def basic_cell():
+    return Cell(pos=(0, 0), box=0)
 
 
-def testCellPrivateValueIsChangedAfterSettingValue():
-    c = Cell(pos=(0, 0), box=0)
-    c.val = 1
-    assert c._val == 1
+def testCellNotesChangeAfterSettingValue(basic_cell):
+    assert basic_cell.notes == {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    basic_cell.val = 1
+    assert basic_cell.notes == {1}
 
 
-def testCellReturnsValueWhenCastToInt():
-    c = Cell(pos=(0, 0), box=0)
-    assert int(c.val) == 0
+def testCellPrivateValueIsChangedAfterSettingValue(basic_cell):
+    basic_cell.val = 1
+    assert basic_cell._val == 1
+
+
+def testCellReturnsValueWhenCastToInt(basic_cell):
+    assert int(basic_cell.val) == 0
+
+
+# Puzzle_Backend
+@fixture
+def empty_puzzle():
+    return Puzzle_Backend()
+
+
+def testPuzzleBackendSetItemChangesCellCorrectly(empty_puzzle):
+    empty_puzzle[0, 0] = 1
+    assert id(empty_puzzle.rows[0][0]) == id(empty_puzzle[0, 0])
+    assert empty_puzzle.rows[0][0].val == empty_puzzle[0, 0].val
+    assert empty_puzzle.rows[0][0].notes == empty_puzzle[0, 0].notes
